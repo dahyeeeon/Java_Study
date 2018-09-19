c9ec64634fddb03dbb9a596906c37ad2afda24ff
dahyeon <koi05@naver.com>
2018-09-19 12:59:13 +0900

    java study

---
+++ javaStudy/src/Step01/comment/MainClass.java
@@ -0,0 +1,18 @@
+//한줄입니다
+/*여러줄주석*/
+
+//빨간색으로 표시되는 단어는 예약어입니다.
+//예약어는 식별자로 못씀
+package Step01.comment; //패키지 예약어를 이용해서 패키지 표시
+
+public class MainClass { //클래스 식별자이름은 main class
+//	run 했을때 실행순서가 들어오는 main메소드 정의법
+	public static void main(String[] args) {
+		System.out.println("Main메소드가 시작됩니다.");
+		System.out.println("하나");
+		System.out.println("두울");
+		System.out.println("세엣");
+		System.out.println("main 메소드가 종료됩니다.");
+	}
+
+}
